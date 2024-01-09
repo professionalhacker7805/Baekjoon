308f3de0f72b99937d402e9bf2df536121dcaa56
seonhukim7805 <101704607+professionalhacker7805@users.noreply.github.com>
2024-01-10 01:15:06 +0900

    [Bronze V] Title: N 찍기, Time: 1100 ms, Memory: 32388 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+
+        for (int i = 1; i < a + 1; i++){
+            System.out.println(i);
+        }
+    }
+}
